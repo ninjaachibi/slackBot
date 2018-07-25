a8f2c8d65488aa8d39af49edd9156fe6a1a7c2aa
Robert L <1998.robert@gmail.com>
2018-07-25 14:34:14 -0700

    Add meeting creation

--- server/calendar.js
+++ server/calendar.js
@@ -10,6 +10,9 @@ export default function gCal(token, info, intent, cb) {
     oAuth2Client.setCredentials(token);
     console.log('in calendar: token is ', token);
     const calendar = google.calendar({version: 'v3', auth: oAuth2Client})
+    let event;
+
+    console.log('data passed to calendar create', info);
 
     if (intent === "reminder_confirm") {
       let endTime = new Date(info.time)
@@ -17,7 +20,7 @@ export default function gCal(token, info, intent, cb) {
       endTime = endTime.toISOString().substring(0, 10)
       let time = info.time.toISOString().substring(0, 10)
 
-      var event = {
+      event = {
         'summary': upper(info.task) ,
         'start': {
           'date': time,
@@ -27,7 +30,23 @@ export default function gCal(token, info, intent, cb) {
         },
       }
     } else if (intent === "meeting_confirm") {
+      let start = info.date;
+      let end = new Date(start);
+      console.log(info.duration);
 
+      event = {
+        'summary': info.task,
+        'start': {
+          'dateTime': '2018-07-25T17:00:00-07:00',
+        },
+        'end': {
+          'dateTime': '2018-07-25T20:00:00-07:00'
+        },
+        'location': info.location,
+        'attendees': [
+          {'email': 'kkpatel@bu.edu'},
+        ]
+      }
     }
 
     calendar.events.insert({
@@ -59,4 +78,22 @@ export default function gCal(token, info, intent, cb) {
 
   function upper(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  function addDuration(startTime, duration) {
+    let total = 0;
+    let {unit, amount} = duration;
+    if(unit.stringValue === 'h') {
+      total = (amount.numberValue * 3600 * 1000);
+    }
+    else if(unit.stringValue === 'min') {
+      total = (amount.numberValue * 60 * 1000);
+    }
+    else if(unit.stringValue === 'day' ) {
+      total = (amount.numberValue * 24 * 3600 * 1000);
+    }
+    else {
+      total = (amount.numberValue * 1800 * 1000);
+    }
+    return total; //make this endTime
   }
